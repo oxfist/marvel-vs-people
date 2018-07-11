743d5d2d0f878fec07e280c340292b2957bc87b1
Andrés Quilodrán <andres.caro.q@gmail.com>
2018-07-10 23:47:39 -0400

    Add a couple seed superheroes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
-Person.create!([
-  {
-    name: 'John Doe',
-    occupation: 'Carpenter',
-    special_ability: 'Fast hammering',
-    quote: 'I\'ll chop you like a tree'
-  }
-])
+Person.create!(
+  [
+    {
+      name: 'John Doe',
+      occupation: 'Carpenter',
+      special_ability: 'Fast hammering',
+      quote: 'I\'ll chop you like a tree'
+    }
+  ]
+)
+
+Superhero.create!(
+  [
+    {
+      name: 'Thanos',
+      character_id: 1009652
+    },
+    {
+      name: 'Spider-Man',
+      character_id: 1009610
+    }
+  ]
+)
