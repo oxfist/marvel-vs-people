c12803a69e08c1688fddc5aadd7278a9bf3616a3
Andrés Quilodrán <andres.caro.q@gmail.com>
2018-07-15 15:59:27 -0400

    Add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_item(text, name)
+    active_status = 'active' if params[:controller] == name
+    tag.a(text, class: "nav-item nav-link mx-3 #{active_status}",
+                href: url_for(controller: name, action: :index))
+  end
 end
